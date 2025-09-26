eefb060c9d6fe7cc4ff4da95406f911828a53c86
5gs_watergod <linshinb1117@gmail.com>
2025-09-26 09:31:43 -0400

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/love-site/sw.js', { scope: '/love-site/' })})}
